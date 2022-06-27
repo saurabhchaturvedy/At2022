60f78dffa5ea86b3c32b74a4d2348b04bb5c307a
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-06-27 21:14:01 +0530

    linked list structure

---
+++ src/main/java/SinglyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,19 @@
+package main.java.SinglyLinkedList;
+
+public class SinglyLinkedList {
+
+    ListNode root;
+
+    private static class ListNode
+    {
+
+        int data;
+        ListNode next;
+
+        ListNode(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+}
